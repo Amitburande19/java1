c3200e207c7630f47b4b4e2dcd64fc65247e667f
Amitburande19 <amitburande777@gmail.com>
2024-02-06 12:54:48 +0530

    added

---
+++ practice.py
@@ -0,0 +1,19 @@
+x =10
+print(x)
+x =20
+print(x)
+q1=8
+q2=4
+print(q1+q2)
+print(q1-q2)
+print(q1*q2)
+print(q1/q2)
+print(q1%q2)
+
+def calculator(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+calculator(8,4)    
