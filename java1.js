3fd5e1dbe993087d26e15ae7fc9b4cbf1fe740dd
Amitburande19 <amitburande777@gmail.com>
2024-02-16 13:51:34 +0530

    added

---
+++ java1.js
@@ -0,0 +1,43 @@
+console.log("Hello")
+let balance = 0
+console.log(balance)
+balance =100
+console.log(balance)
+
+const pi=3.14
+console.log(pi)
+
+
+let q = 8
+let r = 4
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+function calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+calculator(10,5)
+
+
+function additionA (){
+    console.log(9+9)
+}
+additionA()
+
+function additionB(x,y){
+    console.log(x+y)
+}
+additionB(12,3)
+
+function additionC(x,y){
+    return x+y
+}
+let q1 = additionC(13,4)
+console.log(q1)
