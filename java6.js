fd5c78da083bec3af3805475615a8cf93b783b49
Amitburande19 <amitburande777@gmail.com>
2024-02-17 17:49:33 +0530

    added

---
+++ java6.js
@@ -0,0 +1,85 @@
+// while loop
+
+let i =1
+while (i <=3){
+    console.log("hello")
+    i = i+1
+}
+
+let i2 = 1
+while(i2 <=3){
+    console.log(i2)
+    i2 = i2+1
+}
+
+let i3=1
+while(i3 <=5){
+    console.log(i3)
+    i3=i3+1
+}
+
+let i4 =5
+while(i4 >=1){
+    console.log(i4)
+    i4 =i4-1
+}
+
+let i5 =2
+while(i5<=20){
+    console.log(i5)
+    i5 = i5+2
+}
+
+let i6 =50
+while(i6>=5){
+    console.log(i6)
+    i6 = i6-5
+}
+
+//while with break
+
+
+
+
+let i8=1
+while(i8 <=5){
+    if(i8==3){
+    break}
+console.log(i8)
+i8 = i8+1
+}
+
+
+let i9=1
+while(i9 <= 5){
+    console.log(i9)
+    if(i9==3){
+        break
+    }
+    i9=i9+1
+}
+
+
+let i10 =1
+while(i10 <=5){
+    if (i10 ==3){
+        i10++
+        continue
+
+    }
+    console.log(i10)
+    i10 = i10+1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
