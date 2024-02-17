560ab9ae1f747ea5edeeb7e654edd236424f014e
Amitburande19 <amitburande777@gmail.com>
2024-02-17 17:09:23 +0530

    added

---
+++ java5.js
@@ -0,0 +1,56 @@
+for(let i =1 ; i<=3 ; i++){
+    console.log(i)
+}
+
+// for loop with break
+
+for (let i =1 ; i<=5 ; i++){
+    console.log(i)
+    if (i==3){
+        break
+    }
+}
+
+for (let i=5 ; i>=1 ; i--){
+    if(i == 3){
+        break
+    }
+    console.log(i)
+}
+
+
+for (let i =2 ; i <=20 ; i= i+2){
+    if (i == 10){
+        break
+    }
+    console.log(i)
+}
+
+
+// continue with for
+
+
+for (let i =1 ; i <=5 ; i++){
+    if(i==3){
+        continue
+    }
+    console.log(i)
+}
+
+
+for (let i =5 ; i>=1 ; i--){
+    if(i==3){
+        continue
+    }
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
