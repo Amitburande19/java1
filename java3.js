45c1eaf7f350ccb7b6c168e4828bf3c21277bf0b
Amitburande19 <amitburande777@gmail.com>
2024-02-17 10:35:18 +0530

    added

---
+++ java3.js
@@ -0,0 +1,64 @@
+// conditional statement
+
+
+let numR = -17
+if(numR > 0 && numR <= 5){
+    console.log('5% discount')
+}
+else if(numR >5 && numR <=10){
+    console.log('20% discount')
+}
+else if(numR > 10){
+    console.log('30% discont')
+
+}
+else{
+    console.log("incorrect input")
+}
+
+let marks = 92
+if(marks > 90){
+    console.log("grade A")
+}
+if(marks >=75){
+    console.log("grade B")
+}
+if(marks >=60){
+    console.log("grade C")
+}
+
+
+if(marks > 90){
+    console.log("grade A")
+}
+else if(marks >=75){
+    console.log("grade B")
+}
+else if(marks >=60){
+    console.log("grade C")
+}
+
+
+
+let a = 10
+let b = 50
+if(a > b){
+    console.log("a is greater")
+}
+else {
+   console.log("b is greater")   
+}
+
+
+let x1 = 10
+let x2 = 50
+let x3 = 500
+if(x1 > x2 && x1 >x3){
+    console.log("x1 is greater")
+}
+else if(x2 > x1 && x2 >x3){
+    console.log("x2 is greater")
+}
+else {
+    console.log("x3 is greater")
+}
