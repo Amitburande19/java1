d12c1ddf1ae874458f587dcd8771df63033e6e5b
Amitburande19 <amitburande777@gmail.com>
2024-02-17 14:06:24 +0530

    added

---
+++ java4.js
@@ -0,0 +1,66 @@
+// switch case
+
+let city = "pune"
+/*switch(city){
+    case "pune":
+        console.log("MH")
+    case "bhopal":
+        console.log("MP")
+    case "jaipur":
+        console.log("RJ")
+    default :
+        console.log("incorrect city")        
+}
+
+
+let city = "pune"
+switch(city){
+    case "pune":
+        console.log("MH")
+        break
+    case "bhopal":
+        console.log("MP")
+        break
+    case "jaipur":
+        console.log("RJ")
+        break
+    default :
+        console.log("incorrect city")
+}*/
+
+switch(city){
+    case "pune":
+    case "mumbai":
+        console.log("MH")
+        break
+    case "bhopal":
+    case "indore":
+        console.log("MP")
+        break
+    case "jaipur":
+    case "udaipur":
+        console.log("RJ")
+        break
+    default :
+        console.log("incorrect city")
+}
+
+for(let i=1 ; i <=5 ; i++){
+    console.log(i)
+}
+
+for(let i =5 ; i>=1 ; i--){
+    console.log(i)
+}
+
+for(let i=2 ; i<=10 ; i++){
+    console.log(i*2)
+}
+
+for(let i=5 ; i>=1 ; i--){
+    console.log(i)
+}
+
+for(let i=50 ; i>=5 ; i=i-5)
+console.log(i)
+
