be5b7e84fcfbc933212d3f726f1a185c1ec91f82
Amitburande19 <amitburande777@gmail.com>
2024-02-17 09:39:19 +0530

    added

---
+++ java2.js
@@ -0,0 +1,66 @@
+let a =10
+console.log(a)
+console.log(typeof a)
+
+let b = 90.9
+console.log(typeof b)
+
+
+let k =true
+console.log(typeof k)
+
+let first_name = "amit"
+console.log(typeof "amit")
+
+console.log(typeof 10)
+console.log(typeof "10")
+
+
+console.log(2==="2")
+console.log(2!=="2")
+console.log(2===2)
+console.log(2=="2")
+console.log(2!="2")
+console.log(7>3)
+console.log(7<3)
+console.log(7>=7)
+
+
+//and
+
+console.log(7 == 7 && 8 == 8)
+console.log(6 ==='6' && 7===7)
+console.log(6===6 && 7!==7)
+console.log(5 == 4 && 8 == 9)
+
+
+//0r
+
+console.log(7 ==7 || 8 ===8)
+console.log(6 ==='6' || 8==8)
+console.log(6==6 || 7==6)
+console.log(5==4 || 8==9)
+
+// !
+console.log(!(8==8))
+console.log(!(9!=9))
+console.log(!(10==10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
