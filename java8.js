19f2bdd5a12390c00d4ae71eb6a29cbfcf10d6c0
Amitburande19 <amitburande777@gmail.com>
2024-02-18 19:19:54 +0530

    added

---
+++ java8.js
@@ -0,0 +1,83 @@
+//push()
+
+let names = ["chinmay","sham","rm","satish"]
+ let q1 =names.push("sarika")
+ console.log(q1)
+ //console.log(names)
+
+// unshift()
+let q2 = names.unshift("poorva")
+console.log(q2)
+console.log(names)
+
+//pop()
+let fruits = ["apple","mango","banana","grapes"]
+let q3 = fruits.pop()
+console.log(q3)
+//console.log(fruits)
+
+//shift()
+let q4 =fruits.shift()
+console.log(q4)
+console.log(fruits)
+
+//indexof()
+let animals = ["tiger","lion","bear","cat"]
+let q5 = animals.indexOf('lion')
+console.log(q5)
+
+//includes()
+let city = ["banglore","kolkata","chennai","nagpur"]
+let q7 =city.includes("chennai")
+console.log(q7)
+
+//reverse()
+let  q8 =city.reverse()
+console.log(q8)
+console.log(city)
+
+//at()
+
+let country = ["india","pakistan","bangladesh","srilanka"]
+let q9 = country.at(2)
+console.log(q9)
+
+//concat()
+
+let a = [11,22,33]
+let b = [44,55,66]
+let q10 = a.concat(b)
+console.log(q10)
+
+//join()
+
+
+let info = ["chinmay","deshpande",6264316464]
+let q11 = info.join('-')
+console.log(q11)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
