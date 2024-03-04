a2b643fe9c1b1080b1c43a7131b96783d53680ec
Amitburande19 <amitburande777@gmail.com>
2024-03-04 17:09:16 +0530

    added

---
+++ python1.py
@@ -0,0 +1,44 @@
+x = 10
+print(x)
+x = 900
+print(x)
+
+a = 10
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+
+q = 8
+r = 4
+
+print(q+r)
+print(q-r)
+print(q*r)
+print(q/r)
+print(q%r)
+
+
+def calculator(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+calculator(12,4)    
+
+
+
+
+
+
+
+
+
+
+
+
+
