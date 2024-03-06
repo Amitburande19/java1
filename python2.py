59b5250b731dd156e9ad12fd91994912f0b60602
Amitburande19 <amitburande777@gmail.com>
2024-03-06 10:13:49 +0530

    added

---
+++ python2.py
@@ -0,0 +1,86 @@
+# variable
+x = 100
+print(x)
+print(type(x))
+y = 200
+print(y)
+print(type(y))
+
+name = "Ramesh"
+print(name)
+print(type(name))
+
+#operations in python
+print(100+100)
+print(100-100)
+print(100*100)
+print(100/100)
+
+print("100+100")
+print("100-100")
+print("100*100")
+print("100/100")
+
+#comparison
+print(200 > 150)
+print(200 < 150)
+print(200 >= 150)
+print(200 <= 150)
+print(200 == 150)
+
+# assignment operator
+
+#and
+print((100>50)and(200>100))
+print((100<50)and(200>100))
+print((100>50)and(200<100))
+print((100<50)and(200<100))
+
+#or
+
+print((100>50)or(200>100))
+print((100<50)or(200>100))
+print((100>50)or(200<100))
+print((100<50)or(200<100))
+
+
+#not
+#invert the output
+print(not(100<50)or(200<100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python3.py
@@ -0,0 +1,54 @@
+# data types
+x = 100
+print(x)
+print(type(x))
+
+name = 'python'
+print(type(name))
+
+#operators
+a=120
+b=100
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+print(a>b)
+print(a>=b)
+print(a<=b)
+print(a<b)
+print(a==b)
+
+
+#function 
+#calculator
+c=520
+f=720
+#defining a function
+# function with parameter
+def calc(one,two):
+    print(one+two)
+    print(one-two)
+    print(one*two)
+    print(one/two)
+    print(one%two)
+calc(c,f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
