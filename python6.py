81154182cd120410b57c63f38557749252a27c64
Amitburande19 <amitburande777@gmail.com>
2024-03-06 11:02:48 +0530

    added

---
+++ python6.py
@@ -0,0 +1,70 @@
+# loop
+# for x in range(startindex,endindex(not included),stepsize):
+#statements
+#for x in range(1,5):
+#    print(x)
+#for x in range(6):
+ #   print(x)
+#
+#for x in range(2,21,2):
+ #   print(x)
+
+#while loop
+#initialization
+#while(condition):
+#statement
+#increment
+
+#print 1 to 3
+t1 =1
+while(t1<=3):
+    print(t1)
+    t1=t1+1
+
+# print 1 to 5
+t2=1
+while(t2<=5):
+    print(t2)
+    t2=t2+1
+
+#table of 2
+    
+t3=2
+while(t3<=20):
+    print(t3)
+    t3=t3+2
+
+#table of 5 in reverse
+t4=50
+while(t4>4):
+    print(t4)
+    t4=t4-5
+
+#break staement with while loop
+
+t5=1
+while t5<=5 :
+    if t5== 3:
+        break
+    print(t5)
+    t5=t5+1
+
+
+t6=5
+while t6 >=1:
+    if t6 ==3:
+        break
+    print(t6)
+    t6=t6-1   
+
+
+#while loop with continue      
+
+t7 =1
+while t7<= 5:
+    if t7==3:
+       t7=t7+1 
+        continue
+    print(t7)
+    t7=t7+1
+    
