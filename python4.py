7deb7ffc835bad7888fe1e0db0da88d0093bcf95
Amitburande19 <amitburande777@gmail.com>
2024-03-06 10:28:55 +0530

    added

---
+++ python4.py
@@ -0,0 +1,46 @@
+#loops
+# while and for loop
+# for 
+# range()
+# for x in range(start index,end index(not included),stepsize):
+#statement
+
+
+# print(0 to 9)
+for x in range(10): # if startindex not mentioned , zero is default
+    print(x)
+
+
+#print 2 to 9
+    for x in range(2,10):
+        print(x)
+
+# print 1 t0 5
+
+for x in range(1,6):
+    print(x)   
+
+# table of two 
+    for x in range(2,21,2):
+        print(x)
+
+#table of  5 in reverse
+    
+for x in range(50,4,-5):
+    print(x)
+
+#break statement with for loop
+    
+for x in range(1,6):
+    if x==3:
+        break
+    print(x)
+
+for x in range(1,6):
+    print(x)
+    if x==3:
+        break    
+
+
+
+
